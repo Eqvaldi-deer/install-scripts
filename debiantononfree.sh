91f03921baf701f2b7557a8e79fc851bb671f5e3
LOSOperatingsystem <60267789+LOSOperatingsystem@users.noreply.github.com>
2021-03-07 18:50:31 +0100

    Update debiantononfree.sh

--- debiantononfree.sh
+++ debiantononfree.sh
@@ -7,3 +7,4 @@ echo 'deb-src http://deb.debian.org/debian buster-updates main contrib non-free'
 echo 'deb http://security.debian.org/debian-security/ buster/updates main contrib non-free' >> /etc/apt/sources.list
 echo 'deb-src http://security.debian.org/debian-security/ buster/updates main contrib non-free' >> /etc/apt/sources.list
 cat /etc/apt/sources.list
+apt update -y
