31bfc4aa5d45e4c2e719b192f88f9f209c6e7878
LOSOperatingsystem <60267789+LOSOperatingsystem@users.noreply.github.com>
2021-03-07 18:52:57 +0100

    Update debianunstable.sh

--- debianunstable.sh
+++ debianunstable.sh
@@ -3,3 +3,4 @@ rm /etc/apt/sources.list
 echo 'deb http://deb.debian.org/debian/ unstable main contrib non-free' >> /etc/apt/sources.list
 echo 'deb-src http://deb.debian.org/debian/ unstable main contrib non-free' >> /etc/apt/sources.list
 cat /etc/apt/sources.list
+apt update -y
