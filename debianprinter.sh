bb0acb13fd61092d7431f5afc9ae8d452c7c7f96
LOSOperatingsystem <60267789+LOSOperatingsystem@users.noreply.github.com>
2021-03-07 18:49:03 +0100

    Update debianprinter.sh

--- debianprinter.sh
+++ debianprinter.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-apt-get update -y
-apt-get install hplip -y
-apt-get install hplip-gui -y
-apt-get install foomatic-db-compressed-ppds -y
-apt-get install openprinting-ppds -y
-apt-get install policykit-desktop-privileges -y
-apt-get install printer-driver-foo2zjs -y
-apt-get install printer-driver-gutenprint -y
-apt-get install printer-driver-pnm2ppa -y
-apt-get install printer-driver-ptouch -y
-apt-get install printer-driver-pxljr -y
-apt-get install printer-driver-sag-gdi -y
-apt-get install printer-driver-splix -y
-apt-get install cups-filters -y
-apt-get update -y
+apt update -y
+apt install hplip -y
+apt install hplip-gui -y
+apt install foomatic-db-compressed-ppds -y
+apt install openprinting-ppds -y
+apt install policykit-desktop-privileges -y
+apt install printer-driver-foo2zjs -y
+apt install printer-driver-gutenprint -y
+apt install printer-driver-pnm2ppa -y
+apt install printer-driver-ptouch -y
+apt install printer-driver-pxljr -y
+apt install printer-driver-sag-gdi -y
+apt install printer-driver-splix -y
+apt install cups-filters -y
+apt update -y
 apt clean
