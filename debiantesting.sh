afb2d5678b89bc99fccc107965471c6a2bf5aee3
LOSOperatingsystem <60267789+LOSOperatingsystem@users.noreply.github.com>
2021-03-09 00:16:49 +0100

    Create debiantesting.sh

---
+++ debiantesting.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm /etc/apt/sources.list
+echo 'deb http://deb.debian.org/debian/ testing main contrib non-free' >> /etc/apt/sources.list
+echo 'deb-src http://deb.debian.org/debian/ testing main contrib non-free' >> /etc/apt/sources.list
+cat /etc/apt/sources.list
+apt update -y
