975ea2ebe51a3ab00808bb8ff30ee979b09d9932
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2021-03-08 15:56:39 +0100

    Update debiangames.sh

--- debiangames.sh
+++ debiangames.sh
@@ -3,4 +3,7 @@ apt update
 apt install prboom-plus -y
 apt install freedoom -y
 apt install dosbox -y
+#apt install bsdgames -y
+#apt install gnome-2048 -y
+#apt install gnome-breakout -y
 apt update -y
