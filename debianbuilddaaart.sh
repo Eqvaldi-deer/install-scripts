83a6ba811b6af8e6cdde87e4866dba0109a8b149
LOSOperatingsystem <60267789+LOSOperatingsystem@users.noreply.github.com>
2021-03-21 16:34:22 +0100

    Add files via upload

---
+++ debianbuilddaaart.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+apt update 
+apt install git -y
+apt install rustc -y
+mkdir ~/Binary
+cd ~/Binary
+mkdir daaart
+cd ..
+git clone https://github.com/DavidHusicka/daaart.git
+cd daaart/
+cargo build --release
+cd target/release/
+cp daaart daaart.d ~/Binary/daaart
+cd ~/
+rm -rf daaart/
+apt clean
