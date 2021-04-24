5b4f5bbe15142519c433fbdeb3b87144b65d2eac
LOSOperatingsystem <60267789+LOSOperatingsystem@users.noreply.github.com>
2021-04-24 20:51:09 +0200

    Update debiangames.sh

--- debiangames.sh
+++ debiangames.sh
@@ -3,6 +3,7 @@ apt update
 apt install prboom-plus -y
 apt install freedoom -y
 apt install dosbox -y
+#apt install minetest* -y
 #apt install bsdgames -y
 #apt install gnome-2048 -y
 #apt install gnome-breakout -y
