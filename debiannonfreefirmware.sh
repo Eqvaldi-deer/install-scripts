9530c1246571126e7795fe055df3e8a75ccce614
LOSOperatingsystem <60267789+LOSOperatingsystem@users.noreply.github.com>
2021-04-24 19:50:54 +0200

    Update debiannonfreefirmware.sh
    -added firmware and merged widevinerpi.sh

--- debiannonfreefirmware.sh
+++ debiannonfreefirmware.sh
@@ -3,4 +3,12 @@ apt update -y
 apt install firmware-misc-nonfree -y
 apt install firmware-realtek -y
 #apt install firmware-amd-graphics -y
+#apt install raspi-firmware -y
+#Raspberry Pi family GPU firmware and bootloaders
+#apt install firmware-brcm80211 -y
+#Binary firmware for Broadcom/Cypress 802.11 wireless cards
+#apt install firmware-samsung -y
+#Binary firmware for Samsung MFC video codecs Exynos SoCs
+#apt install libwidevinecdm0 -y
+#WideVine. Enables DRM for Netflix, HULU, HBO MAX, Spotify, Amazon Prime video Disney Plus or any site that requires WideVine to stream content
 apt update -y
