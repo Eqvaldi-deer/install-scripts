3b3421c5adc6260b197adbe70bf3b9d2a83ab938
LOSOperatingsystem <60267789+LOSOperatingsystem@users.noreply.github.com>
2021-05-24 22:14:40 +0200

    TEST Arch Linux
    - submited by Beangreen247
    (Warning beta needs testing)

---
+++ archlinuxgnome.sh.sh
@@ -0,0 +1,5 @@
+sudo pacman -Syyuu
+sudo pacman -S  file-roller gdm gnome-calculator  eog  gnome-disk-utility  gnome-system-monitor gnome-terminal  gnome-menus gnome-settings-daemon mutter networkmanager gnome-shell gnome-session gnome-terminal nautilus xorg xorg-xinit xterm nano gnome-control-center
+sudo systemctl enable gdm
+sudo localectl set-locale LANG="en_US.UTF-8"
+sudo pacman -Syy
