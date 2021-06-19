488765258316423b24162ddb0a577e8cd538e0d4
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2021-06-19 11:47:41 +0200

    Update debiannonfreefirmware.sh

--- debiannonfreefirmware.sh
+++ debiannonfreefirmware.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt update -y
 apt install firmware-misc-nonfree -y
+apt install firmware-linux-free -y
 apt install firmware-realtek -y
 #apt install firmware-amd-graphics -y
 #apt install intel-microcode -y
