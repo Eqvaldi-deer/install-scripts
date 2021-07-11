577f416f67730301ae78669779c21f32c55195ec
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2021-07-11 21:27:49 +0200

    Bare minimum Version

---
+++ debianXFCEBM.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+apt update
+apt install xfce4 xfce4-terminal -y
+apt install file-roller -y
+apt install galculator -y
+apt install libmtp-runtime -y
+apt install light-locker -y
+apt install neofetch -y
+apt install lightdm-gtk-greeter-settings -y
+apt install xorg -y
+apt install lightdm -y
+apt install htop -y
+apt install usb-modeswitch -y
+apt install genisoimage -y
+apt install inputattach -y
+apt install xfce4-notifyd -y
+apt install xfce4-power-manager -y
+apt install xfce4-whiskermenu-plugin -y
+apt install xfce4-power-manager-plugins -y
+apt install gparted -y
+apt install xarchiver -y
+apt install p7zip -y
+apt install zip unzip -y
+apt install libgtk-3-dev libgtk3-perl -y
+apt update -y
+apt upgrade -y
+apt update -y
+apt clean
