2c309df0f4d3ace8833df8e315b06993e2956528
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2021-08-07 20:59:19 +0200

    Update debiangnome.sh

--- debiangnome.sh
+++ debiangnome.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 apt update
+apt install dialog -y
+
+dialog --msgbox "DO NOT RUN THESE SCRIPTS ON UBUNTU BASED DISTROS" 0 0
+
 apt install gnome-shell -y
 apt install gnome-session -y
 apt install gnome-terminal -y
