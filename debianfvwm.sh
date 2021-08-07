268315cc3a6023a3ddcfcbdfdce948499dfabeaf
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2021-08-07 20:58:23 +0200

    Update debianfvwm.sh

--- debianfvwm.sh
+++ debianfvwm.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 apt update -y
+apt install dialog -y
+
+dialog --msgbox "DO NOT RUN THESE SCRIPTS ON UBUNTU BASED DISTROS" 0 0
+
 apt install fvwm -y
 apt install thunar -y
 apt install light-locker -y
