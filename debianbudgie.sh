4488958f19fa3142e6019112ab8a49235dbcb609
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2021-08-07 20:57:42 +0200

    Update debianbudgie.sh

--- debianbudgie.sh
+++ debianbudgie.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 apt update
+apt install dialog -y
+
+dialog --msgbox "DO NOT RUN THESE SCRIPTS ON UBUNTU BASED DISTROS" 0 0
+
 apt install budgie-core -y
 apt install gnome-session-bin -y
 apt install gnome-menus -y
