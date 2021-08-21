4d4743cf098c849fd8a6f2c978f980e87e76ea2a
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2021-08-21 21:59:02 +0200

    Update debiantononfree.sh
    -Updated to bullseye

--- debiantononfree.sh
+++ debiantononfree.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 rm /etc/apt/sources.list
-echo 'deb http://deb.debian.org/debian buster main contrib non-free' >> /etc/apt/sources.list
-echo 'deb-src http://deb.debian.org/debian buster main contrib non-free' >> /etc/apt/sources.list
-echo 'deb http://deb.debian.org/debian buster-updates main contrib non-free' >> /etc/apt/sources.list
-echo 'deb-src http://deb.debian.org/debian buster-updates main contrib non-free' >> /etc/apt/sources.list
-echo 'deb http://security.debian.org/debian-security/ buster/updates main contrib non-free' >> /etc/apt/sources.list
-echo 'deb-src http://security.debian.org/debian-security/ buster/updates main contrib non-free' >> /etc/apt/sources.list
+echo 'deb http://deb.debian.org/debian bullseye main contrib non-free' >> /etc/apt/sources.list
+echo 'deb-src http://deb.debian.org/debian bullseye main contrib non-free' >> /etc/apt/sources.list
+echo 'deb http://deb.debian.org/debian bullseye-updates main contrib non-free' >> /etc/apt/sources.list
+echo 'deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free' >> /etc/apt/sources.list
+echo 'deb http://security.debian.org/debian-security/ bullseye-security main contrib non-free' >> /etc/apt/sources.list
+echo 'deb-src http://security.debian.org/debian-security/ bullseye-security main contrib non-free' >> /etc/apt/sources.list
 cat /etc/apt/sources.list
 apt update -y
