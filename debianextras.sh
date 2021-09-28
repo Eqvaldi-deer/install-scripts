cd279d95aeeaeb4c0d02674be098cdb539084fab
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2021-09-28 18:27:48 +0200

    Update debianextras.sh

--- debianextras.sh
+++ debianextras.sh
@@ -3,11 +3,11 @@ apt update -y
 #apt install gufw -y
 apt install handbrake -y
 apt install audacious -y
-apt install audacity -y
+#apt install audacity -y
 apt install putty -y
 apt install gimp -y
 apt install vlc-plugin-fluidsynth -y
-apt install fluidsynth -y
+#apt install fluidsynth -y
 #apt install mixxx -y
 #apt install ardour -y
 #apt install muse -y
