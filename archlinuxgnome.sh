40d35dc9f6232add2334c461a663f9f51574adf1
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2021-10-10 15:13:09 +0200

    Delete archlinuxgnome.sh

--- archlinuxgnome.sh
+++
@@ -1,6 +0,0 @@
-sudo pacman -Syyuu
-sudo pacman -S bash-completion file-roller chromium ghostscript libmtp gdm gnome-calculator eog htop gnome-icon-theme usb_modeswitch blueman gnome-disk-utility gvfs gedit system-config-printer transmission-gtk mesa tumbler gparted xarchiver p7zip zip unzip soundconverter ffmpeg linuxconsole mesa-demos base-devel curl freealut openal sdl2_mixer sdl2 lib32-sdl2 lib32-sdl2_mixer gtk3 gtk4 gnome-system-monitor gnome-terminal  gnome-menus gnome-settings-daemon mutter networkmanager gnome-shell gnome-session gnome-terminal nautilus xorg xorg-xinit ipython xterm nano gnome-control-center gnome-backgrounds audacity audacious audacious-plugins gufw gimp putty fluidsynth
-#sudo pacman -S xf86-video-ati
-sudo systemctl enable gdm
-sudo localectl set-locale LANG="en_US.UTF-8"
-sudo pacman -Syy
