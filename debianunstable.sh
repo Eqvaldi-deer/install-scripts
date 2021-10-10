8c7fb06bb884c1baa6f2e700598d1d3b66257e75
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2021-10-10 15:11:42 +0200

    Delete debianunstable.sh

--- debianunstable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm /etc/apt/sources.list
-echo 'deb http://deb.debian.org/debian/ unstable main contrib non-free' >> /etc/apt/sources.list
-echo 'deb-src http://deb.debian.org/debian/ unstable main contrib non-free' >> /etc/apt/sources.list
-cat /etc/apt/sources.list
-apt update -y
