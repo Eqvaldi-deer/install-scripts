ec7862580a1ddb703094e9baca23330f9220af66
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2021-11-10 16:38:18 +0100

    Update ArmbiandebianLXDE.sh

--- ArmbiandebianLXDE.sh
+++ ArmbiandebianLXDE.sh
@@ -16,6 +16,7 @@ apt install xarchiver -y
 apt install usb-modeswitch -y
 apt install p7zip -y
 apt install zip unzip -y
+apt install uuid-runtime -y
 apt install mesa-utils-extra -y
 apt install gvfs-backends -y
 apt install gvfs-common -y
