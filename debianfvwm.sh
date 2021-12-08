da30dd6e4be4379ed7fa7e329e8879017d7a93bb
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2021-12-08 17:28:53 +0100

    Update debianfvwm.sh

--- debianfvwm.sh
+++ debianfvwm.sh
@@ -54,6 +54,14 @@ apt install libalut-dev -y
 apt install libsdl2-dev -y
 apt install libsdl2-mixer-dev -y
 apt install libgtk-3-dev libgtk3-perl -y
+apt install ffmpeg -y
+apt install libavcodec58 -y
+apt install exfalso -y
+apt install font-manager -y
+apt install libjpeg-dev -y
+apt install gnome-screenshot -y
+apt install catfish -y
+apt install thunar-archive-plugin -y
 apt update -y
 apt upgrade -y
 rm /etc/apt/sources.list.d/vscode.list
