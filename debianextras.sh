44144dd6aa6815c63e479c86c023685d812507e2
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2022-01-21 05:03:24 +0100

    Update debianextras.sh

--- debianextras.sh
+++ debianextras.sh
@@ -7,7 +7,7 @@ apt install audacious -y
 apt install putty -y
 apt install gimp -y
 apt install vlc-plugin-fluidsynth -y
-#apt install fluidsynth -y
+apt install fluidsynth -y
 #apt install mixxx -y
 #apt install ardour -y
 apt-get update -y
