753f2e4c76d664096568f2c7008f9f18f5fbafeb
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2022-02-27 19:54:43 +0100

    Add files via upload

---
+++ flatpaktest.sh
@@ -0,0 +1,4 @@
+apt install flatpak -y
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.chromium.Chromium
+flatpak run org.chromium.Chromium
