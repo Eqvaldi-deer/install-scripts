6023abb30514cb86894d6a34137225e21dfc52ec
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2022-04-10 21:03:49 +0200

    Update ArmbiandebianLXDE.sh

--- ArmbiandebianLXDE.sh
+++ ArmbiandebianLXDE.sh
@@ -4,9 +4,8 @@ apt install dialog -y
 
 dialog --msgbox "DO NOT RUN THESE SCRIPTS ON UBUNTU BASED DISTROS" 0 0
 
-apt install openbox -y
-apt install lxsession -y
-apt install lxde-common -y
+apt install openbox lxsession lxde-common -y
+apt install lxappearance -y
 apt install xfce4-notifyd -y
 apt install xfce4-power-manager -y
 apt install xfce4-power-manager-plugins -y
