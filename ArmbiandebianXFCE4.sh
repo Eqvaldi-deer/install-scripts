99cee8ed04b8c465382b8588b68b31d3b0a185d1
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2022-04-25 13:56:34 +0200

    Update ArmbiandebianXFCE4.sh

--- ArmbiandebianXFCE4.sh
+++ ArmbiandebianXFCE4.sh
@@ -66,7 +66,7 @@ apt install exfalso -y
 apt install flac -y
 apt install font-manager -y
 apt install libjpeg-dev -y
-apt install gnome-screenshot -y
+apt install xfce4-screenshooter -y
 apt install catfish -y
 apt install thunar-archive-plugin -y
 #apt install libmali-xu4-x11-gbm-fbdev
