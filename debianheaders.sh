d752c02e853cee6d501f025438ada9ce394e5204
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2022-05-25 04:05:15 +0200

    Delete debianheaders.sh

--- debianheaders.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#apt install linux-headers-amd64 -y
-#apt install linux-headers-686 -y
-#apt install linux-headers-arm64 -y
-#apt install linux-headers-armmp .y
-#apt install linux-headers-marvell -y
-#apt install linux-headers-686-pae -y
