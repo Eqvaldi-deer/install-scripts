f8ef793a18de44898226dabf7bed55dc4e3991b6
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2022-05-25 04:06:28 +0200

    Delete flatpaktest.sh

--- flatpaktest.sh
+++
@@ -1,4 +0,0 @@
-apt install flatpak -y
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.chromium.Chromium
-flatpak run org.chromium.Chromium
