c3d5a6d48db7a305a245b2c1e2d01470263b0265
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2023-05-07 20:21:09 +0200

    Update other2.sh

--- other2.sh
+++ other2.sh
@@ -1,4 +1,4 @@
-            cmd=(dialog --keep-tite --menu "Select NVIDIA GPU Driver Version:" 22 76 16)
+            cmd=(dialog --keep-tite --menu "Select GPU Driver:" 22 76 16)
 
             options=(1 "NVIDIA-Latest (525)"
                      2 "Intel-nonfree (INTEL)"
