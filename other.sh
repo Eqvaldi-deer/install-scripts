bf5e6831bde6a636ccdfdcd2c62302f9c90be165
Eqvaldi-deer <60267789+Eqvaldi-deer@users.noreply.github.com>
2023-10-06 01:44:26 +0200

    Update other.sh

--- other.sh
+++ other.sh
@@ -11,7 +11,7 @@ for choice in $choices
 do
     case $choice in
         1)
-            apt install gufw handbrake audacious audacity putty gimp dosbox milkytracker cmake spek -y
+            apt install gufw handbrake audacious audacity putty gimp dosbox milkytracker libsdl2-net-dev cmake spek -y
             cmd=(dialog --keep-tite --menu "Select Web browser:" 22 76 16)
 
             options=(1 "chromium"
@@ -47,7 +47,7 @@ do
             done
             ;;
         2)
-            apt install gufw handbrake audacious audacity putty gimp vlc-plugin-fluidsynth fluidsynth dsda-doom freedoom dosbox milkytracker cmake spek -y
+            apt install gufw handbrake audacious audacity putty gimp vlc-plugin-fluidsynth fluidsynth dsda-doom freedoom dosbox milkytracker libsdl2-net-dev cmake spek -y
             apt purge minetest-server
             cmd=(dialog --keep-tite --menu "Select Web browser:" 22 76 16)
 
@@ -84,7 +84,7 @@ do
             done
             ;;
         3)
-            apt install gufw handbrake audacious audacity putty gimp vlc-plugin-fluidsynth fluidsynth mixxx ardour dsda-doom freedoom dosbox minetest* bsdgames gnome-2048 gnome-breakout milkytracker cmake spek -y
+            apt install gufw handbrake audacious audacity putty gimp vlc-plugin-fluidsynth fluidsynth mixxx ardour dsda-doom freedoom dosbox minetest* bsdgames gnome-2048 libsdl2-net-dev gnome-breakout milkytracker cmake spek -y
             cmd=(dialog --keep-tite --menu "Select Web browser:" 22 76 16)
 
             options=(1 "chromium"
