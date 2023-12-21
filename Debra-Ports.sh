f7d1d3ae0f600f37f013e91c856b4822faa21c6b
Eqvaldi <60267789+Eqvaldi-deer@users.noreply.github.com>
2023-12-21 23:54:20 +0100

    Add files via upload

--- Debra-Ports.sh
+++ Debra-Ports.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 mkdir ./Debra-Ports
 cd ./Debra-Ports
-apt update 
-apt install build-essential dialog git nasm libgl1-mesa-dev libsdl2-dev flac libflac-dev libvpx-dev libgtk2.0-dev freepats ninja-build qtbase5-dev qtbase5-private-dev qtbase5-dev-tools qttools5-dev -y
-apt clean
+ 
 
 dialog --msgbox "DO NOT RUN Debra-Ports ON UBUNTU BASED DISTROS" 0 0
 
@@ -12,13 +10,13 @@ cmd=(dialog --keep-tite --menu "Select a Port:" 22 76 16)
 options=(1 "Dhewm3"
          2 "Eduke32"
          3 "Darkplaces"
-         4 "Darkplaces-nexuiz"
+         4 "Minetest"
          5 "Ioq3"
          6 "worldofpadman"
          7 "Yamagi Quake II"
          8 "Duckstation (PSX emu.)"
          9 "iortcw"
-	 10 "Flycast (Dreamcast emu.) (WIP)"
+	 10 "DSDA-Doom"
 	 11 "Exit")
 
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
@@ -30,7 +28,7 @@ do
           git clone https://github.com/dhewm/dhewm3.git
 	  cd dhewm3/
 	  cmake ./neo/
-	  make
+	  make -j$(nproc)
 	  cd ..
 	  cd ..
           bash ./Debra-Ports.sh
@@ -38,7 +36,7 @@ do
         2)
           git clone https://voidpoint.io/terminx/eduke32.git
 	  cd eduke32/
-	  make USE_OPENGL=0 POLYMER=0 USE_LIBVPX=0 OPTLEVEL=2 WITHOUT_GTK=1
+	  make -j$(nproc) USE_OPENGL=0 POLYMER=0 USE_LIBVPX=0 OPTLEVEL=2 WITHOUT_GTK=1
 	  cd ..
 	  cd ..
           bash ./Debra-Ports.sh
@@ -46,15 +44,19 @@ do
         3)
           git clone https://github.com/DarkPlacesEngine/darkplaces.git
 	  cd darkplaces/
-	  make sdl-release
+	  make -j$(nproc) sdl-release
 	  cd ..
 	  cd ..
           bash ./Debra-Ports.sh
             ;;
         4)
-          git clone https://github.com/DarkPlacesEngine/darkplaces.git
-	  cd darkplaces/
-	  make sdl-nexuiz
+          git clone --depth 1 https://github.com/minetest/minetest.git
+	  cd minetest
+	  git clone --depth 1 https://github.com/minetest/minetest_game.git games/minetest_game
+ 	  git clone --depth 1 https://github.com/minetest/irrlicht.git lib/irrlichtmt
+	  git clone https://codeberg.org/SumianVoice/backroomtest.git games/backroomtest
+	  cmake . -DRUN_IN_PLACE=TRUE
+	  make -j$(nproc)
 	  cd ..
 	  cd ..
           bash ./Debra-Ports.sh
@@ -62,7 +64,7 @@ do
         5)
           git clone https://github.com/ioquake/ioq3.git
 	  cd ioq3/
-	  make
+	  make -j$(nproc)
 	  cd ..
 	  cd ..
           bash ./Debra-Ports.sh
@@ -72,18 +74,18 @@ do
 	  unzip *.zip
 	  rm -rf ./*.zip
 	  cd worldofpadman-1.6.2/
-	  make
+	  make -j$(nproc)
 	  cd ..
 	  cd ..
           bash ./Debra-Ports.sh
             ;;
         7)
           #git clone https://github.com/yquake2/yquake2.git
-	  wget https://github.com/yquake2/yquake2/archive/refs/tags/QUAKE2_8_10.zip
-	  unzip *.zip
+	  wget https://github.com/yquake2/yquake2/archive/refs/tags/QUAKE2_8_30.zip
+   	  unzip *.zip
 	  rm -rf ./*.zip
-	  cd yquake2-QUAKE2_8_10/
-	  make
+   	  cd yquake2-QUAKE2_8_30/
+	  make -j$(nproc)
 	  cd ..
 	  cd ..
           bash ./Debra-Ports.sh
@@ -92,28 +94,33 @@ do
           git clone https://github.com/stenzek/duckstation.git -b dev
 	  cd duckstation/
 	  cmake ./
-	  make
+	  make -j$(nproc)
 	  cd ..
 	  cd ..
           bash ./Debra-Ports.sh
             ;;
         9)
           git clone https://github.com/iortcw/iortcw.git
-	  cd ioq3/
-	  make
+	  cd iortcw/
+   	  cd SP/
+      	  make -j$(nproc)
+	  cd ..
+          cd MP/
+	  make -j$(nproc)
+   	  cd ..
 	  cd ..
 	  cd ..
           bash ./Debra-Ports.sh
             ;;
         10)
-          apt install libminiupnpc-dev liblua5.4-dev spirv-tools openssl libao-dev -y
-	  apt install mame-tools --no-install-recommends -y
-	  git clone https://github.com/flyinghead/flycast.git
-	  cd flycast/
+	  git clone https://github.com/kraflab/dsda-doom.git
+	  cd ./dsda-doom/
+          cd ./prboom2/
 	  cmake ./
-	  make
+	  make -j$(nproc)
 	  cd ..
 	  cd ..
+    	  cd ..
           bash ./Debra-Ports.sh
             ;;
         11)
